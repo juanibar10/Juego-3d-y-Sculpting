8393d4c9b95237f5adaad553f4c98c9383aeed41
juanibar10 <47811666+juanibar10@users.noreply.github.com>
2019-04-04 19:36:34 +0200

    Proyecto

---
+++ Prueba interaccion objetos/Assets/Inventario/Scripts/ControladorInventario.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class ControladorInventario : MonoBehaviour
+{
+    public GameObject inventario;
+    public GameObject notas;
+    private bool abierto = false;
+    private bool abiertoNotas = false;
+
+   
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I) && !abiertoNotas)
+        {
+            abierto = !abierto;
+            GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().enabled = !abierto;
+        }
+
+        GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().m_MouseLook.SetCursorLock(!abierto);
+        inventario.SetActive(abierto);
+
+        if (Input.GetKeyDown(KeyCode.O) && !abierto)
+        {
+            abiertoNotas = !abiertoNotas;
+            GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().enabled = !abiertoNotas;
+        }
+        GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().m_MouseLook.SetCursorLock(!abiertoNotas);
+        notas.SetActive(abiertoNotas);
+    }
+
+    public void CerrarInventario()
+    {
+        abierto = !abierto;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().m_MouseLook.SetCursorLock(true);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().enabled = !abierto;
+    }
+
+    public void CerrarNotas()
+    {
+        abiertoNotas = !abiertoNotas;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().m_MouseLook.SetCursorLock(true);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().enabled = !abiertoNotas;
+    }
+
+    public void botonNotas()
+    {
+        abiertoNotas = !abiertoNotas;
+        abierto = !abierto;
+    }
+
+    public void botonInventario()
+    {
+        abierto = !abierto;
+        abiertoNotas = !abiertoNotas;
+    }
+
+
+}
